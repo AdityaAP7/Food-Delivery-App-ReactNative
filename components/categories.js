25fb9b9cae35d6be25f581e550d4a7cf1664a9a2
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-04-25 23:30:31 +0530

    Update categories.js

--- components/categories.js
+++ components/categories.js
@@ -37,7 +37,16 @@ const [activeCategory,setActiveCategory] = useState(null)
     </View>
   );
 })}
-
+1
+  2
+    3
+      4
+        5
+          6
+            7
+              8
+                9
+                  10
       </ScrollView>
     </View>
   );
